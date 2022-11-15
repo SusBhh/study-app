71e2115e103ae3e6f0e3465ee3a2677116d27cf9
Yanna Lin <linyanna@yahoo.com>
2022-11-15 00:35:36 -0500

    Connect activities with buttons

--- app/src/main/java/com/example/tidbit_astudyapp/ProfileActivity.java
+++ app/src/main/java/com/example/tidbit_astudyapp/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.tidbit_astudyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+    public void onBackPressed(View view) {
+        Log.d("Example: ", "Clicked back");
+        finish();
     }
 }
